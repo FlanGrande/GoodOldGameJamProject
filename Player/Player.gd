b4d8d9b817ad988769048d0ec74b8b632471b158
FlanGrande <francisjgrande@gmail.com>
2022-06-10 21:23:05 +0100

    More refinement
    Polished a couple of details about the movement.

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,26 +2,24 @@ extends KinematicBody2D
 
 onready var screen_size = get_viewport_rect().size
 
-enum State { WALK, FLY, ATTACK, BRAKING, FALL }
+enum State { WALK, FLY, ATTACK, BRAKING, FALL, DEATH }
 
 const GRAVITY = 400.0
 const INITIAL_WALK_SPEED = 30
-const WALK_SPEED_INCREMENT = 20
+const WALK_SPEED_INCREMENT = 10
 const MAX_SPEED = 990
 const FLY_SPEED = 180
-const SPRITE_WIDTH = 64
+const SPRITE_WIDTH = 32
 const SPRITE_HEIGHT = 64
-const SPAWN_POSITION = Vector2(0, 100)
+const SPAWN_POSITION = Vector2(539, 597)
 var velocity = Vector2(INITIAL_WALK_SPEED, 100) # default: right
 
-var is_alive = true
 var current_state = State.WALK
 var current_animation = "walk"
 var animation_speed_factor = 0.24
 var animation_speed = 0
 
 func _ready():
-	is_alive = true
 	current_state = State.WALK
 	velocity = Vector2(INITIAL_WALK_SPEED, 100)
 	$AnimationPlayer.play(current_animation, -1, animation_speed)
@@ -35,37 +33,38 @@ func _physics_process(delta):
 	elif velocity.x > 0:
 		$AnimatedSprite.flip_h = false
 	
-	if not is_alive:
-		die()
+	if current_state != State.DEATH:
+		if is_on_floor():
+			update_state(State.WALK)
+		else:
+			if(velocity.y > 0):
+				update_state(State.FALL)
+		if Input.is_action_pressed("left"):
+			velocity.x -= WALK_SPEED_INCREMENT
+			if velocity.x > 0 and current_state != State.FLY and current_state != State.FALL:
+				$AnimationPlayer.play("brake", -1, 0.0)
+			if current_state == State.FLY or current_state == State.FALL:
+				$AnimatedSprite.flip_h = true
+		elif Input.is_action_pressed("right"):
+			velocity.x +=  WALK_SPEED_INCREMENT
+			if velocity.x < 0 and current_state != State.FLY and current_state != State.FALL:
+				$AnimationPlayer.play("brake", -1, 0.0)
+			if current_state == State.FLY or current_state == State.FALL:
+				$AnimatedSprite.flip_h = false
+		
+		if Input.is_action_pressed("up"):
+			velocity.y = -FLY_SPEED
+			update_state(State.FLY)
+			if(not $AnimationPlayer.current_animation == "fly"):
+				$AnimationPlayer.play("fly", -1, 2.0)
 	
-	if is_on_floor():
-		update_state(State.WALK)
-	else:
-		if(velocity.y > 0):
-			update_state(State.FALL)
+		velocity.x = min(MAX_SPEED, velocity.x)
+		velocity.x = max(-MAX_SPEED, velocity.x)
+		animation_speed = velocity.x * pow(animation_speed_factor, 2)
 	
-	if Input.is_action_pressed("left"):
-		velocity.x -= WALK_SPEED_INCREMENT
-		if velocity.x > 0 and current_state != State.FLY and current_state != State.FALL:
-			$AnimationPlayer.play("brake", -1, 0.0)
-	elif Input.is_action_pressed("right"):
-		velocity.x +=  WALK_SPEED_INCREMENT
-		if velocity.x < 0 and current_state != State.FLY and current_state != State.FALL:
-			$AnimationPlayer.play("brake", -1, 0.0)
-	
-	if Input.is_action_pressed("up"):
-		velocity.y = -FLY_SPEED
-		update_state(State.FLY)
-		if(not $AnimationPlayer.current_animation == "fly"):
-			$AnimationPlayer.play("fly", -1, 2.0)
-	
-	velocity.x = min(MAX_SPEED, velocity.x)
-	velocity.x = max(-MAX_SPEED, velocity.x)
-	animation_speed = velocity.x * pow(animation_speed_factor, 2)
-	
-	move_and_slide(velocity, Vector2(0, -1))
-	keep_in_boundaries()
-	check_collisions(delta)
+		move_and_slide(velocity, Vector2(0, -1))
+		keep_in_boundaries()
+		check_collisions(delta)
 
 func keep_in_boundaries():
 	position.x = wrapf(position.x, -SPRITE_WIDTH, screen_size.x + SPRITE_WIDTH / 2)
@@ -103,6 +102,9 @@ func process_state(delta):
 	
 	if current_state == State.FALL:
 		fall(delta)
+	
+	if current_state == State.DEATH:
+		death()
 
 func walk():
 	velocity.y = 1
@@ -113,7 +115,7 @@ func walk():
 		$AnimationPlayer.play("walk", -1, animation_speed)
 
 func fly(delta):
-	velocity.y += delta * GRAVITY # delta?
+	velocity.y += delta * GRAVITY
 
 func attack():
 	pass
@@ -122,8 +124,19 @@ func braking():
 	pass
 
 func fall(delta):
-	velocity.y += delta * GRAVITY # delta?
+	velocity.y += delta * GRAVITY
 	$AnimationPlayer.play("fall", -1, 0)
 
-func die():
+func death():
+	velocity = Vector2(0, 20)
+	move_and_slide(velocity, Vector2(0, -1))
+
+func _on_Area2D_body_entered(body):
+	update_state(State.DEATH)
+	$AnimationPlayer.play("death", -1, 3.0)
+	$DeathTimer.start()
+
+func _on_DeathTimer_timeout():
+	update_state(State.WALK)
 	position = SPAWN_POSITION
+	velocity = Vector2(INITIAL_WALK_SPEED, 100)
