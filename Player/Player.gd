db018b68ffc7f036c6e7fc27b36b1cad813b0973
FlanGrande <francisjgrande@gmail.com>
2022-06-11 12:57:12 +0100

    More tweaks to movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,28 +4,29 @@ onready var screen_size = get_viewport_rect().size
 
 enum State { WALK, FLY, ATTACK, BRAKING, FALL, DEATH }
 
-const GRAVITY = 400.0
-const INITIAL_WALK_SPEED = 30
-const WALK_SPEED_INCREMENT = 10
-const MAX_SPEED = 400
-const FLY_SPEED = 100
+const GRAVITY = 2
+const INITIAL_WALK_SPEED = 0
+const WALK_SPEED_INCREMENT = 2
+const MAX_SPEED = 200
+const FLY_SPEED = 40
 const SPRITE_WIDTH = 32
 const SPRITE_HEIGHT = 64
 const SPAWN_POSITION = Vector2(539, 597)
 var velocity = Vector2(INITIAL_WALK_SPEED, 100) # default: right
 
-var current_state = State.WALK
+var current_state = State.FALL
 var current_animation = "walk"
 var animation_speed_factor = 0.24
 var animation_speed = 0
 
 func _ready():
-	current_state = State.WALK
+	current_state = State.FALL
 	velocity = Vector2(INITIAL_WALK_SPEED, 100)
 	$AnimationPlayer.play(current_animation, -1, animation_speed)
 
 # Rework to go in increments and remain the same while no input is given
 func _physics_process(delta):
+	print(current_state)
 	process_state(delta)
 	
 	if velocity.x < 0 and current_state != State.FLY and current_state != State.FALL:
@@ -34,11 +35,8 @@ func _physics_process(delta):
 		$AnimatedSprite.flip_h = false
 	
 	if current_state != State.DEATH:
-		if is_on_floor():
-			update_state(State.WALK)
-		else:
-			if(velocity.y > 0):
-				update_state(State.FALL)
+		update_state(State.FALL)
+		
 		if Input.is_action_pressed("left"):
 			velocity.x -= WALK_SPEED_INCREMENT
 			if velocity.x > 0 and current_state != State.FLY and current_state != State.FALL:
@@ -52,19 +50,23 @@ func _physics_process(delta):
 			if current_state == State.FLY or current_state == State.FALL:
 				$AnimatedSprite.flip_h = false
 		
-		if Input.is_action_pressed("up"):
-			velocity.y = -FLY_SPEED
+		if Input.is_action_just_pressed("up"):
+			velocity.y += -FLY_SPEED
 			update_state(State.FLY)
 			if(not $AnimationPlayer.current_animation == "fly"):
-				$AnimationPlayer.play("fly", -1, 2.0)
+				$AnimationPlayer.play("fly", -1, 4.0)
 	
 		velocity.x = min(MAX_SPEED, velocity.x)
 		velocity.x = max(-MAX_SPEED, velocity.x)
+		velocity.y = min(MAX_SPEED, velocity.y)
+		velocity.y = max(-MAX_SPEED, velocity.y)
 		animation_speed = velocity.x * pow(animation_speed_factor, 2)
-	
-		move_and_slide(velocity, Vector2(0, -1))
-		keep_in_boundaries()
+		
 		check_collisions(delta)
+		move_and_collide(velocity * delta)
+		keep_in_boundaries()
+	
+	
 
 func keep_in_boundaries():
 	position.x = wrapf(position.x, -SPRITE_WIDTH, screen_size.x + SPRITE_WIDTH / 2)
@@ -76,13 +78,13 @@ func check_collisions(delta):
 	# TEMP solution
 	if collision_info:
 		var collider = collision_info.get_collider()
+		
 		if collider.is_in_group("wall"):
-			if floor(rad2deg(collision_info.get_angle())) == 90:
-				velocity.x = 0
-				
-				if current_state == State.WALK:
-					$AnimationPlayer.seek(0.0)
-					$AnimationPlayer.play("walk", -1, 0.0)
+			if floor(rad2deg(collision_info.get_angle())) == 0:
+				update_state(State.WALK)
+			else:
+				velocity = velocity.bounce(collision_info.normal) / 2.0
+				update_state(State.FALL)
 
 func update_state(state):
 	current_state = state
@@ -107,7 +109,7 @@ func process_state(delta):
 		death()
 
 func walk():
-	velocity.y = 1
+	velocity.y = 0
 	
 	if velocity.x < 0:
 		$AnimationPlayer.play("walk", -1, -animation_speed)
@@ -115,7 +117,7 @@ func walk():
 		$AnimationPlayer.play("walk", -1, animation_speed)
 
 func fly(delta):
-	velocity.y += delta * GRAVITY
+	pass#velocity.y += delta * GRAVITY
 
 func attack():
 	pass
@@ -124,8 +126,9 @@ func braking():
 	pass
 
 func fall(delta):
-	velocity.y += delta * GRAVITY
-	$AnimationPlayer.play("fall", -1, 0)
+	if $AnimationPlayer.current_animation != "fly" and velocity.y > 0:
+		$AnimationPlayer.play("fall", -1, 0)
+	velocity.y += GRAVITY
 
 func death():
 	velocity = Vector2(0, 20)
@@ -137,6 +140,6 @@ func _on_Area2D_body_entered(body):
 	$DeathTimer.start()
 
 func _on_DeathTimer_timeout():
-	update_state(State.WALK)
+	update_state(State.FALL)
 	position = SPAWN_POSITION
 	velocity = Vector2(INITIAL_WALK_SPEED, 100)
